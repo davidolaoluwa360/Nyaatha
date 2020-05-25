536dae5c60f4f17674f33b9e92fea2b4f4d42bde
davidolaoluwa360 <d.atebisun@alustudent.com>
2020-05-25 16:40:06 +0200

    just finish the who we serve page

--- resources/views/Who we serve/index.blade.php
+++ resources/views/Who we serve/index.blade.php
@@ -2,37 +2,37 @@
 
 @section('title', 'Nyaatha|Who We Serve')
 
-@section('who we serve container')
+@section('whoweserve')
     <section class='whoweservegeneraloutercontainer'>
         <div class='whoweservemainheaderselector'>
-            <span>NYAATA EDUCATION ENDOWMENT FUND</span>
-            <span>Home/Who We Serve</span>
+            <span class='whoweserveboldlargefont'>NYAATA EDUCATION ENDOWMENT FUND</span>
+            <span class='whoweserveboldmediumfont'>Home/Who We Serve</span>
         </div>
         <div class='whowearegeneralinnercontainerselector'>
             <div class='Dwhoweare'>
                 <h1>Who We Serve</h1>
                 <p>The Assumption Sisters Of Eldoret (Nyaatha Foundation) have worked with the street program for several years
-                     however the Nyaatha is the initiative of the assumption sisters of Eldoret 
+                     however the Nyaatha is the initiative of the assumption sisters of Eldoret
                      to improve the lives of disadvantaged children/young people.</p>
 
                 <p>The experience of working with this program is that street children have
                      the potential of being productive in society if supported.
-                      The life of the young people in our community has been so distracted that leading 
+                      The life of the young people in our community has been so distracted that leading
                       into the school dropout and even some have never gone to school.</p>
 
-                <p>The social problems have contributed towards children forming bad groups in the society, e.g. drug abuse, 
-                    immorality and theft cases not only among the street children but also children 
+                <p>The social problems have contributed towards children forming bad groups in the society, e.g. drug abuse,
+                    immorality and theft cases not only among the street children but also children
                     from an impoverished background.</p>
             </div>
             <div class='Aligntogetherwhoweareselector'>
                 <div class='whoweservegroups'>
                     <h1>A.S.E. Nyaatha Education Endowent</h1>
-                    <p>Majority of the people in the communities we serve live below the poverty line. 
+                    <p>Majority of the people in the communities we serve live below the poverty line.
                         They only get what is barely sufficient for their daily survival, which has contributed
                          to the organization to go extra miles in also looking food for the children who
                           are malnourished in the community.</p>
 
-                    <p>The organization move from one company/organizations to another searching for 
+                    <p>The organization move from one company/organizations to another searching for
                         the support of food, shelter and finally education support, not only companies
                          but also from good wishers.</p>
                 </div>
@@ -40,18 +40,18 @@
                 <div class='whoweservegroups'>
                     <h1>Target Groups</h1>
                     <p>Most of the young people we meet both in church, on the streets and in the community are unable
-                        to meet their educational needs and personal effects. The majority of the populations we serve 
-                        are small scale farmers and also people who affected by tribal clashes during post-election violence 
+                        to meet their educational needs and personal effects. The majority of the populations we serve
+                        are small scale farmers and also people who affected by tribal clashes during post-election violence
                         that contributed many children being left by their parents. Whose output cannot sustain the family
                          as well as undertaking the caring? The need to reach these young people is so great.</p>
                 </div>
                 <div class='whoweservegroups'>
                     <h1>Nyaatha A Ray Of Hope</h1>
-                    <p>The result of the Fund is to empower the disadvantaged children 
+                    <p>The result of the Fund is to empower the disadvantaged children
                         and give them a Ray of Hope through the education to be able to stand
                          on their own and also become a ray of hope for others. Some of the young
-                          men who are now in colleges or joining colleges were street boys, and now 
-                          they have a Ray of Hope because of the mercy shown to them by others through 
+                          men who are now in colleges or joining colleges were street boys, and now
+                          they have a Ray of Hope because of the mercy shown to them by others through
                           the support of their education.</p>
                 </div>
             </div>
--- resources/views/layouts/Whowesearvelayouts.blade.php
+++ resources/views/layouts/Whowesearvelayouts.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="content-type" type='html/text' charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
-        <link rel="stylesheet" type="text/css" media="screen" href="{{URL::to('CSS/whoweworkfor.css')}}">
+        <link rel="stylesheet" type="text/css" media="screen" href="{{URL::to('CSS/whoweserve.css')}}">
         <link rel="stylesheet" type="text/css" media="screen" href="{{URL::to('CSS/generalheader.css')}}">
         <script src="https://kit.fontawesome.com/f7712776f7.js" crossorigin="anonymous"></script>
     </head>
@@ -12,7 +12,7 @@
       @include('partials.masterheader')
 
       <div class="who we serve container">
-        @yield('who we serve')
+        @yield('whoweserve')
       </div>
 
       @include('partials.masterfooter')
